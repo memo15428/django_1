5a4b07ae0fcb773b7448324b14a47620fec7d92d
Miguel Mogollon <61074055+memo15428@users.noreply.github.com>
2020-04-08 00:43:00 -0500

    modificacion de URLs

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import (
+    TemplateView,
+)
+
+class IndexView(TemplateView):
+    print("---------------------prueba de URL-------------------------")
+
 # Create your views here.
