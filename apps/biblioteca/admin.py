55c0ca9f1f62aca2e8b584ab940add54e9f67e25
Miguel Mogollon <61074055+memo15428@users.noreply.github.com>
2020-04-08 22:57:14 -0500

    models libros

--- apps/biblioteca/admin.py
+++ apps/biblioteca/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Autor
+from .models import Autor, Libros
 
 admin.site.register(Autor)
+admin.site.register(Libros)
--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -4,7 +4,15 @@ from django.db import models
 
 class Autor(models.Model):
     nombre = models.CharField("Nombres", max_length=80)
-    nacionalidad = models.CharField("Nacionalidad", blank=True, max_length=100)
+    nacionalidad = models.CharField(blank=True, max_length=100)
 
     def __str__(self):
         return self.nombre
+
+class Libros(models.Model):
+    title = models.CharField('titulo', blank=True, max_length=150)
+    resume = models.TextField('Resumen', blank=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
